dab984c543dbbfee03b77fe7bc080e949bac9fa0
yahooghoub-tech <yahooghoub@gmail.com>
2026-08-07 12:39:13 +0330

    update website

--- nazem.js
+++ nazem.js
@@ -9,9 +9,10 @@ SUPABASE_KEY
 
 console.log("Supabase connected");
 
-
 let recognition;
 let isListening=true;
+
+let speechBuffer="";
 
 
 const micStatus=document.getElementById("micStatus");
@@ -48,11 +49,359 @@ const students=[
     {name:"عماد مظلومی نیا",className:"ششم-1"},
     {name:"آرتین محمدبیگی",className:"ششم-1"},
     {name:"میثم نگهداری",className:"ششم-1"},
-    {name:"مازیار نگهداری",className:"ششم-1"}
+    {name:"مازیار نگهداری",className:"ششم-1"},
+    
+    
+    // لیست دانش‌آموزان کلاس سوم-1
+
+
+    {name:"ساتیار امیری",className:"سوم-1"},
+    {name:"پارسا تقی زاده",className:"سوم-1"},
+    {name:"رایان جمشیدی",className:"سوم-1"},
+    {name:"رادین جمشیدی",className:"سوم-1"},
+    {name:"کارن جهانی",className:"سوم-1"},
+    {name:"بهراد حسینی نژاد",className:"سوم-1"},
+    {name:"نویان خدامرادی",className:"سوم-1"},
+    {name:"فرداد خدایاری",className:"سوم-1"},
+    {name:"آدرین سلاجقه",className:"سوم-1"},
+    {name:"شهریار سلگی",className:"سوم-1"},
+    {name:"آراد شریفی",className:"سوم-1"},
+    {name:"آرین صفری",className:"سوم-1"},
+    {name:"رایان عیسی زاده",className:"سوم-1"},
+    {name:"آرشان عیوض نژاد",className:"سوم-1"},
+    {name:"کارن کاردان",className:"سوم-1"},
+    {name:"رادمان کامکار",className:"سوم-1"},
+    {name:"آرمان کرمیان",className:"سوم-1"},
+    {name:"رهام ماندگارمقدم",className:"سوم-1"},
+    {name:"رادمان مرادیان نژاد",className:"سوم-1"},
+    {name:"مهراد ناصری",className:"سوم-1"},
+    {name:"آریا نصیرمحمدی",className:"سوم-1"},
+    {name:"آریا نعمتی",className:"سوم-1"},
+    
+    // ==========================
+// دانش‌آموزان کلاس سوم-2
+// ==========================
+
+    {name:"مهرسام اسدرخت",className:"سوم-2"},
+    {name:"آریا اسماعیلی",className:"سوم-2"},
+    {name:"امیرعلی اکبرآبادی",className:"سوم-2"},
+    {name:"دانیال اکبری مهر",className:"سوم-2"},
+    {name:"سید محمدامیر انوری",className:"سوم-2"},
+    {name:"جانیار ایرجی",className:"سوم-2"},
+    {name:"ایلیا آغباشلو",className:"سوم-2"},
+    {name:"پندار خوش طینتان",className:"سوم-2"},
+    {name:"آروین سوری",className:"سوم-2"},
+    {name:"آبهان شهرتی",className:"سوم-2"},
+    {name:"رادین طلایی پناه",className:"سوم-2"},
+    {name:"آرمان عبدی",className:"سوم-2"},
+    {name:"سامیار عزیزی مقدم",className:"سوم-2"},
+    {name:"رادین فرشچین",className:"سوم-2"},
+    {name:"ایلیا قاسمی",className:"سوم-2"},
+    {name:"محمد مهدی کاظمی",className:"سوم-2"},
+    {name:"پویا کرمی",className:"سوم-2"},
+    {name:"نیکان گنجه",className:"سوم-2"},
+    {name:"حسین محمدی مهر",className:"سوم-2"},
+    {name:"ویهان منصوری",className:"سوم-2"},
+    {name:"میثم نظری",className:"سوم-2"},
+
+    // ==========================
+// دانش‌آموزان کلاس اول-1
+// ==========================
+    {name:"آرسام ابهری",className:"اول-1"},
+    {name:"هومان باوی",className:"اول-1"},
+    {name:"سامراد دمیرچلی",className:"اول-1"},
+    {name:"آرش عبدی",className:"اول-1"},
+    {name:"میلان غلامی آبادانی",className:"اول-1"},
+    {name:"آیهان قاسمی",className:"اول-1"},
+    {name:"ویهان کاملی",className:"اول-1"},
+    {name:"رایان محرابی",className:"اول-1"},
+    {name:"آرشا محمودی",className:"اول-1"},
+    {name:"رایان مقدم",className:"اول-1"},
+    {name:"سبدصدرا منصورزاده",className:"اول-1"},
+    {name:"جاوید نصرالهی",className:"اول-1"},
+    
+    // ==========================
+// دانش‌آموزان کلاس چهارم-1
+// ==========================
+
+    {name:"محمدطاها احمدی",className:"چهارم-1"},
+    {name:"آریا آزاد پیما",className:"چهارم-1"},
+    {name:"رادمهر بشیری",className:"چهارم-1"},
+    {name:"مهراد بیاتی",className:"چهارم-1"},
+    {name:"پوریا توکلیان",className:"چهارم-1"},
+    {name:"رادین حسنی",className:"چهارم-1"},
+    {name:"اوتانا درویشی",className:"چهارم-1"},
+    {name:"امیرعباس دهقان",className:"چهارم-1"},
+    {name:"سام زندمقدم",className:"چهارم-1"},
+    {name:"مهراد سفارزاد",className:"چهارم-1"},
+    {name:"نویان علیشاهی",className:"چهارم-1"},
+    {name:"مهراد عموحسن",className:"چهارم-1"},
+    {name:"کوروش قاسمی",className:"چهارم-1"},
+    {name:"محمدحسین قرابیگلو",className:"چهارم-1"},
+    {name:"رهام لطفی",className:"چهارم-1"},
+    {name:"امیرعلی ناعمی",className:"چهارم-1"},
+    
+    // ==========================
+// دانش‌آموزان کلاس اول-2
+// ==========================
+    {name:"یونا ایازیان",className:"اول-2"},
+    {name:"علی اینانلو گنجی",className:"اول-2"},
+    {name:"رادوین برادری هم پا",className:"اول-2"},
+    {name:"مانیاد رسام",className:"اول-2"},
+    {name:"امیرعلی صفاوردی",className:"اول-2"},
+    {name:"آروین عباسی",className:"اول-2"},
+    {name:"شاهان فراهانی",className:"اول-2"},
+    {name:"حسین فخیمی شایسته",className:"اول-2"},
+    {name:"صدرا قنبری",className:"اول-2"},
+    {name:"رادمهر کارشناس",className:"اول-2"},
+    {name:"رادمان نادمی",className:"اول-2"},
+    {name:"کسری نعمت زاده",className:"اول-2"},
+    {name:"نامی هاشمی",className:"اول-2"},
+    
+    // ==========================
+// دانش‌آموزان کلاس پنجم-3
+// ==========================
+
+
+    {name:"آراد احمدیان",className:"پنجم-3"},
+    {name:"رادین امیری",className:"پنجم-3"},
+    {name:"آران جهانبانی",className:"پنجم-3"},
+    {name:"آوش حسینبکی",className:"پنجم-3"},
+    {name:"علی خادم",className:"پنجم-3"},
+    {name:"ماهان دیلمقانی زاده",className:"پنجم-3"},
+    {name:"سپهر رجاء",className:"پنجم-3"},
+    {name:"آرسیین رضایی",className:"پنجم-3"},
+    {name:"آرسیس رضایی",className:"پنجم-3"},
+    {name:"همایون رفیعی",className:"پنجم-3"},
+    {name:"میعاد زمانی",className:"پنجم-3"},
+    {name:"راستین زهدی",className:"پنجم-3"},
+    {name:"مهبد شکری",className:"پنجم-3"},
+    {name:"آرتا شیرازی",className:"پنجم-3"},
+    {name:"محمدامین صیادنورد",className:"پنجم-3"},
+    {name:"بردیا ضیایی",className:"پنجم-3"},
+    {name:"سید آیین عظیمی",className:"پنجم-3"},
+    {name:"مهرسام علیزاده",className:"پنجم-3"},
+    {name:"بردیا قلعه گلاب",className:"پنجم-3"},
+    {name:"رادمان مرسلی",className:"پنجم-3"},
+    {name:"آریا میرزاده",className:"پنجم-3"},
+    {name:"هومان نصرتی",className:"پنجم-3"},
+    {name:"رادمان نوروزی",className:"پنجم-3"},
+    {name:"محمدرضا یارلو",className:"پنجم-3"},
+    
+    // ==========================
+// دانش‌آموزان کلاس چهارم-3
+// ==========================
+    {name:"آرسام حسینی",className:"چهارم-3"},
+    {name:"لیام رحمانی",className:"چهارم-3"},
+    {name:"ایلیا سبزپوش",className:"چهارم-3"},
+    {name:"ماهان سمنارشاد",className:"چهارم-3"},
+    {name:"آروین عابدی",className:"چهارم-3"},
+    {name:"ایلیا عرشی",className:"چهارم-3"},
+    {name:"آراد عطاییان",className:"چهارم-3"},
+    {name:"آرتین علمی",className:"چهارم-3"},
+    {name:"مهرسام غضنفری",className:"چهارم-3"},
+    {name:"نیکان فرجی",className:"چهارم-3"},
+    {name:"دانیال کشاورز",className:"چهارم-3"},
+    {name:"کارن کوهی",className:"چهارم-3"},
+    {name:"علی گرجایی",className:"چهارم-3"},
+    {name:"رایان مننظری",className:"چهارم-3"},
+    {name:"آرین نیک پی",className:"چهارم-3"},
+    {name:"ویهان وهابی",className:"چهارم-3"},
+    
+    // ==========================
+// دانش‌آموزان کلاس چهارم-2
+// ==========================
+
+    {name:"سید محمد اجاقی",className:"چهارم-2"},
+    {name:"امیرمحمد امانی",className:"چهارم-2"},
+    {name:"کارن امانی",className:"چهارم-2"},
+    {name:"بنیامین حسین زاده",className:"چهارم-2"},
+    {name:"سید سینا حسینی",className:"چهارم-2"},
+    {name:"مبین دمرچلی",className:"چهارم-2"},
+    {name:"سپهر ذوالفقاری",className:"چهارم-2"},
+    {name:"علیسام رمضانی",className:"چهارم-2"},
+    {name:"کیان علیا",className:"چهارم-2"},
+    {name:"آرتین کوچاری",className:"چهارم-2"},
+    {name:"سام لوحی خسروشاهی",className:"چهارم-2"},
+    {name:"برکان محمدخانی",className:"چهارم-2"},
+    {name:"مهراد منصفی",className:"چهارم-2"},
+    {name:"سورنا منصوری",className:"چهارم-2"},
+    {name:"ارسام مهری نژاد",className:"چهارم-2"},
+    
+    // ==========================
+// دانش‌آموزان کلاس سوم-3
+// =========================
+    {name:"فرهام احمدی نژاد",className:"سوم-3"},
+    {name:"آرشا تابع",className:"سوم-3"},
+    {name:"نیکان تورجی",className:"سوم-3"},
+    {name:"پارسا تهامی پور",className:"سوم-3"},
+    {name:"حافظ جعفربیگی",className:"سوم-3"},
+    {name:"رادوین دزیانی",className:"سوم-3"},
+    {name:"نویان رنجبر",className:"سوم-3"},
+    {name:"مهدیار رهبر",className:"سوم-3"},
+    {name:"رادمان سلیمانیه",className:"سوم-3"},
+    {name:"کیان سهرابی",className:"سوم-3"},
+    {name:"شایان شاوردین",className:"سوم-3"},
+    {name:"آرشا طیبی",className:"سوم-3"},
+    {name:"امیرپارسا عباسی",className:"سوم-3"},
+    {name:"ارس علوی",className:"سوم-3"},
+    {name:"فرهام فرقانی",className:"سوم-3"},
+    {name:"آریانمهر محمداکبری",className:"سوم-3"},
+    {name:"کیاراد مرادی",className:"سوم-3"},
+    {name:"بردیا میرشفیعی",className:"سوم-3"},
+    // ==========================
+// دانش‌آموزان کلاس اول-3
+// ==========================
+
+    {name:"مهربد بلند همت",className:"اول-3"},
+    {name:"آدار جهانبانی",className:"اول-3"},
+    {name:"علیسان جهانی",className:"اول-3"},
+    {name:"سامیار سلیمی",className:"اول-3"},
+    {name:"لیام شریفی",className:"اول-3"},
+    {name:"آرشا شریفی",className:"اول-3"},
+    {name:"عرفان علیزاده",className:"اول-3"},
+    {name:"رادین عموحسن",className:"اول-3"},
+    {name:"سپهراد مرادی",className:"اول-3"},
+    {name:"آدرین نجارزاده",className:"اول-3"},
+    {name:"کیاراد هوشی",className:"اول-3"},
+    {name:"یزدان یوسفی",className:"اول-3"},
+    
+    // ==========================
+// دانش‌آموزان کلاس دوم-2
+// ==========================
+    {name:"دیان احمدی",className:"دوم-2"},
+    {name:"رایبد آتش بهار",className:"دوم-2"},
+    {name:"آرمان تقی ماهانی",className:"دوم-2"},
+    {name:"مهرسام جلائیان",className:"دوم-2"},
+    {name:"مهدیار حسنی",className:"دوم-2"},
+    {name:"مانی حسینی",className:"دوم-2"},
+    {name:"آروین خانی",className:"دوم-2"},
+    {name:"رایان خمسه",className:"دوم-2"},
+    {name:"آرتا خدابنده",className:"دوم-2"},
+    {name:"کوروش درگاهی",className:"دوم-2"},
+    {name:"رادین سعیدی",className:"دوم-2"},
+    {name:"رهام سلطانزاده",className:"دوم-2"},
+    {name:"پرهام صادقی",className:"دوم-2"},
+    {name:"آرتین طاهری مقدم",className:"دوم-2"},
+    {name:"آبتین عابدی",className:"دوم-2"},
+    {name:"آیریک عباسی",className:"دوم-2"},
+    {name:"محمدامین قدرتی",className:"دوم-2"},
+    {name:"باربد قصابی",className:"دوم-2"},
+    {name:"مانی کریمی",className:"دوم-2"},
+    {name:"وبهان کمالپور",className:"دوم-2"},
+    {name:"رهام منصوری",className:"دوم-2"},
+    {name:"محمد پارسا نبوی زاده",className:"دوم-2"},
+    {name:"رادین هادیان",className:"دوم-2"},
+    // ==========================
+// دانش‌آموزان کلاس دوم-1
+// ==========================
+
+    {name:"رادمان احمدی طباطبایی",className:"دوم-1"},
+    {name:"آراد بزرگی",className:"دوم-1"},
+    {name:"آراد ثبوتی",className:"دوم-1"},
+    {name:"رادمان حیدری",className:"دوم-1"},
+    {name:"شنتیا درویشی",className:"دوم-1"},
+    {name:"امیررضا دوادانگه",className:"دوم-1"},
+    {name:"رایان رادمنش",className:"دوم-1"},
+    {name:"رامان رحیمی",className:"دوم-1"},
+    {name:"رهام صناعت گر",className:"دوم-1"},
+    {name:"سامیار طاهرزاده",className:"دوم-1"},
+    {name:"شاهان علی آبادی",className:"دوم-1"},
+    {name:"بردیا فاضل",className:"دوم-1"},
+    {name:"یونا فیض دار",className:"دوم-1"},
+    {name:"آرتا قلخانی",className:"دوم-1"},
+    {name:"نیما کاکاسلطانی",className:"دوم-1"},
+    {name:"ویهان لک",className:"دوم-1"},
+    {name:"ارسلان معینی",className:"دوم-1"},
+    {name:"ماهور منصوری",className:"دوم-1"},
+    {name:"نیهاد نجاری",className:"دوم-1"},
+    {name:"یونا هاتف",className:"دوم-1"},
+    
+    // ==========================
+// دانش‌آموزان کلاس ششم-2
+// ==========================
+   {name:"فرهاد احمدی نژاد",className:"ششم-2"},
+    {name:"روهان حیدری",className:"ششم-2"},
+    {name:"دانیال زارع قمشه",className:"ششم-2"},
+    {name:"ماهان زند",className:"ششم-2"},
+    {name:"آدرین سعیدی",className:"ششم-2"},
+    {name:"محمدعلی شهبازی",className:"ششم-2"},
+    {name:"متین عباسی",className:"ششم-2"},
+    {name:"لرستانی عماد",className:"ششم-2"},
+    {name:"آرشام عمرانی",className:"ششم-2"},
+    {name:"رادین فروغی",className:"ششم-2"},
+    {name:"پدرام قربانی",className:"ششم-2"},
+    {name:"آرکا کامیار",className:"ششم-2"},
+    {name:"رایان کلانتری",className:"ششم-2"},
+    {name:"دانا کاظمی",className:"ششم-2"},
+    {name:"عرفان مقدم لو",className:"ششم-2"},
+    {name:"ماهان مجیدی",className:"ششم-2"},
+    {name:"حسام مظلومی نیا",className:"ششم-2"},
+    {name:"آدرین مهدی زاده",className:"ششم-2"},
+    {name:"سپنتا محبی",className:"ششم-2"},
+    {name:"فراز نعمت طلب",className:"ششم-2"},
+    {name:"امیرحسین وقار",className:"ششم-2"},
+    
+    // ==========================
+// دانش‌آموزان کلاس پنجم-1
+// ==========================
+
+
+    {name:"محمدمهدی ابیض",className:"پنجم-1"},
+    {name:"ماهان اجتهادی",className:"پنجم-1"},
+    {name:"سامیار اسکندری",className:"پنجم-1"},
+    {name:"آرشا افتخاری",className:"پنجم-1"},
+    {name:"کارن آتش بهار",className:"پنجم-1"},
+    {name:"شهراد چم",className:"پنجم-1"},
+    {name:"مهراد حسینی",className:"پنجم-1"},
+    {name:"رهام روشنی صبح",className:"پنجم-1"},
+    {name:"پارسا سعیدی نیا",className:"پنجم-1"},
+    {name:"مهراد سفیدگران",className:"پنجم-1"},
+    {name:"محمد سیف الهی",className:"پنجم-1"},
+    {name:"سروش شمسیان",className:"پنجم-1"},
+    {name:"آتیلا صفری",className:"پنجم-1"},
+    {name:"رادین عباسی",className:"پنجم-1"},
+    {name:"آرشام فتحی زاده",className:"پنجم-1"},
+    {name:"آرسام کاظمی",className:"پنجم-1"},
+    {name:"امیرعلی کریمی راد",className:"پنجم-1"},
+    {name:"محمد کهتری",className:"پنجم-1"},
+    {name:"ماهان مختاری",className:"پنجم-1"},
+    {name:"آرشین مقدسی",className:"پنجم-1"},
+    {name:"امیرمهدی میرزاآقایی",className:"پنجم-1"},
+    {name:"فربد ناطقی",className:"پنجم-1"},
+    {name:"کارن نعمتی",className:"پنجم-1"},
+    {name:"فرهام هاشمی",className:"پنجم-1"},
+    {name:"یاسین یوسفی",className:"پنجم-1"},
+    // ==========================
+// دانش‌آموزان کلاس پنجم-2
+// ==========================
+    {name:"کیان امامقلی",className:"پنجم-2"},
+    {name:"مهراد امانی پور",className:"پنجم-2"},
+    {name:"هیوا بهرامی",className:"پنجم-2"},
+    {name:"اهورا تاتلاری",className:"پنجم-2"},
+    {name:"کیان چابکی",className:"پنجم-2"},
+    {name:"آرتین خدمتلو",className:"پنجم-2"},
+    {name:"ویهان داداشعلی",className:"پنجم-2"},
+    {name:"ماجد رسولی",className:"پنجم-2"},
+    {name:"طاها زرگر",className:"پنجم-2"},
+    {name:"ایلیا عباسی",className:"پنجم-2"},
+    {name:"یزدان عبدالهی",className:"پنجم-2"},
+    {name:"مهرسام علایی",className:"پنجم-2"},
+    {name:"آرن فلاح",className:"پنجم-2"},
+    {name:"مهراد فلاحتی",className:"پنجم-2"},
+    {name:"امیرحافظ قمی",className:"پنجم-2"},
+    {name:"امیرعلی کازرانی",className:"پنجم-2"},
+    {name:"کیاراد کاظمی",className:"پنجم-2"},
+    {name:"یاسان لشگری نژاد",className:"پنجم-2"},
+    {name:"آریا لطیفی",className:"پنجم-2"},
+    {name:"برسام محمداسماعیل",className:"پنجم-2"},
+    {name:"سامیار مرادی",className:"پنجم-2"},
+    {name:"سورنا مرادی",className:"پنجم-2"},
+    {name:"یاسین مولایی",className:"پنجم-2"},
+    {name:"محمد صدرا میرصادقی",className:"پنجم-2"},
+    {name:"رادین یاهو",className:"پنجم-2"}
     
     ];
-
-
 
 if("webkitSpeechRecognition" in window){
 
@@ -155,11 +504,11 @@ recognition.onresult=function(event){
     
     
     
-    speechText.innerText=text;
-    
-    
-    
-    findMultipleStudents(text);
+    speechBuffer += " " + text;
+
+speechText.innerText = speechBuffer;
+
+findMultipleStudents(speechBuffer);
     
     
     
@@ -167,25 +516,155 @@ recognition.onresult=function(event){
     
     
     
-    function findMultipleStudents(text){
-    
-    
-    students.forEach(student=>{
-    
-    
-    if(text.includes(student.name)){
-    
-    
-    sendTeacherMessage(student);
-    
-    
-    }
-    
-    
-    });
-    
-    
-    }
+    function normalizeText(text){
+
+        return text
+        .replace(/\s+/g,"")
+        .trim();
+        
+        }
+        
+        
+        
+        function similarity(a,b){
+        
+        let longer =
+        a.length>b.length ? a:b;
+        
+        let shorter =
+        a.length>b.length ? b:a;
+        
+        
+        let longerLength=longer.length;
+        
+        
+        if(longerLength===0){
+        return 1;
+        }
+        
+        
+        let distance =
+        levenshteinDistance(
+        longer,
+        shorter
+        );
+        
+        
+        return (
+        longerLength-distance
+        )/
+        longerLength;
+        
+        
+        }
+        
+        
+        
+        function levenshteinDistance(a,b){
+        
+        let matrix=[];
+        
+        
+        for(let i=0;i<=b.length;i++){
+        
+        matrix[i]=[i];
+        
+        }
+        
+        
+        for(let j=0;j<=a.length;j++){
+        
+        matrix[0][j]=j;
+        
+        }
+        
+        
+        for(let i=1;i<=b.length;i++){
+        
+        for(let j=1;j<=a.length;j++){
+        
+        
+        if(
+        b[i-1]===a[j-1]
+        ){
+        
+        matrix[i][j]=
+        matrix[i-1][j-1];
+        
+        
+        }
+        else{
+        
+        matrix[i][j]=Math.min(
+        
+        matrix[i-1][j-1]+1,
+        
+        matrix[i][j-1]+1,
+        
+        matrix[i-1][j]+1
+        
+        );
+        
+        }
+        
+        
+        }
+        
+        }
+        
+        
+        return matrix[b.length][a.length];
+        
+        }
+        
+        
+        
+        function findMultipleStudents(text){
+        
+        
+        let voiceText =
+        normalizeText(text);
+        
+        
+        
+        students.forEach(student=>{
+        
+        
+        let studentName =
+        normalizeText(
+        student.name
+        );
+        
+        
+        
+        let score =
+        similarity(
+        voiceText,
+        studentName
+        );
+        
+        
+        
+        if(score>=0.75){
+        
+        
+        console.log(
+        "تشخیص:",
+        student.name,
+        score
+        );
+        
+        
+        sendTeacherMessage(student);
+        
+        
+        }
+        
+        
+        });
+        
+        
+        }
     
     
     
